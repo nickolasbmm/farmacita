d5b19704e6b99bceb81094ed26b81a3a9240e9f2
Montanha <arieivovatsug@gmail.com>
2020-11-21 23:12:17 -0300

    adicionei pagina de cadastro de med

--- frontend/_js/script.js
+++ frontend/_js/script.js
@@ -12,3 +12,59 @@ function mascara_cpf(i){
     if (v.length == 11) i.value += "-";
 
 }
+
+function testa_cpf(campo_cpf){
+    valor_do_campo = campo_cpf.value
+    cpf = valor_do_campo.replace(/[.-]/g,'')
+    
+    soma1 = 0
+    for(i = 0; i < 9; i++){
+        soma1 += cpf[i] * (10-i)
+    }
+    resto1 = ((soma1 * 10) % 11)%10
+
+    soma2 = 0
+    for(i = 0; i < 10; i++){
+        soma2 += cpf[i] * (11-i)
+    }
+    resto2 = ((soma2 * 10) % 11)%10
+
+    valido = true
+
+    if(resto1 == cpf[9] & resto2 == cpf[10]){
+        valido = true
+    }
+    else{
+        valido = false
+    }
+    if(/^(.)\1*$/.test(cpf)){
+        valido = false
+    }
+
+    if(!valido){
+        $("#cpf").css("border", "1px solid red")
+        console.log(soma1, resto1, soma2, resto2, valido, /^(.)\1*$/.test(cpf))
+    }
+    else{
+        $("#cpf").css("border", "1px solid #ced4da")
+    }
+
+};
+String.prototype.formatUnicorn = String.prototype.formatUnicorn ||
+function () {
+    "use strict";
+    var str = this.toString();
+    if (arguments.length) {
+        var t = typeof arguments[0];
+        var key;
+        var args = ("string" === t || "number" === t) ?
+            Array.prototype.slice.call(arguments)
+            : arguments[0];
+
+        for (key in args) {
+            str = str.replace(new RegExp("\\{" + key + "\\}", "gi"), args[key]);
+        }
+    }
+
+    return str;
+};
