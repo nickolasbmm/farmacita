3007a2dfa53bcf14385a480824bf2ed959bc96e8
Samarasrs <smr.samara.srs@gmail.com>
2020-11-28 15:54:30 -0300

    atualização

--- farmacita/cadastro_medicamentos/views.py
+++ farmacita/cadastro_medicamentos/views.py
@@ -1,10 +1,10 @@
+
 from django.shortcuts import render
 from .models import medicamento
 
 # Create your views here.
 def cadmedicamentos(request):
-    droga = medicamento.objects.values_list('principio_ativo')
-
+    
     if request.method == "POST":
         if request.POST.get("save"):
             p = request.POST
@@ -12,4 +12,6 @@ def cadmedicamentos(request):
             item.save()
             print(item.nome_medicamento)
 
-    return render(request,'pagina_cadastro_medicamento.html',{"droga":droga})
+
+    return render(request,'pagina_cadastro_medicamento.html',{})
+
