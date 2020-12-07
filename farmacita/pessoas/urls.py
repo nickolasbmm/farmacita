8b34b9b04f4475de5d6b7d667a75055a0844e969
nandaasrodrigues <50269159+nandaasrodrigues@users.noreply.github.com>
2020-12-07 19:04:03 -0300

    Delete urls.py

--- farmacita/pessoas/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    #path('cursos',views.show_courses, name = 'show_courses'),
-    path('authentication',views.authentication, name = 'authentication'),
-    path('principal',views.principal, name = 'principal'),
-]
