06eff93415e06946e79ba344a5729f915da1d04f
nickolasbmm <nickolasbmm44@gmail.com>
2020-12-08 19:02:52 -0300

    entrada_estoque implementado falta sugestoes html

--- farmacita/controle_estoque/models.py
+++ farmacita/controle_estoque/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 from cadastro_medicamentos.models import medicamento
+from pessoas.models import fornecedor
 # Create your models here.
 
 class lote_medicamento(models.Model):
     id_lote_medicamento = models.AutoField(primary_key=True)
     id_medicamento = models.ForeignKey(medicamento,on_delete=models.PROTECT)
+    id_fornecedor = models.ForeignKey(fornecedor,on_delete=models.PROTECT)
     preco = models.DecimalField(max_digits=40,decimal_places=2)
-    preco_com_desconto = models.DecimalField(max_digits=40,decimal_places=2)
-    quantidade = models.IntegerField()
-    quantidade_por_caixa = models.IntegerField()
-    concentracao = models.CharField(max_length=100)
-    formato = models.CharField(max_length=100)
-    marca = models.CharField(max_length=100)
+    quantidade_de_caixas = models.CharField(max_length=100)
+    quantidade_por_caixa = models.CharField(max_length=100)
+    dosagem = models.CharField(max_length=100)
+    data_de_validade = models.DateTimeField(auto_now=False,auto_now_add=False,null=False)
     industria_farmaceutica = models.CharField(max_length=100)
-    status = models.CharField(max_length=100)
     excluido = models.BooleanField(default=False)
--- farmacita/controle_estoque/views.py
+++ farmacita/controle_estoque/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
+from .models import lote_medicamento
+from pessoas.models import fornecedor
+from cadastro_medicamentos.models import medicamento
 
 # Create your views here.
 
 def entrada_estoque(request):
+
+    if request.method == "POST":
+        p = request.POST
+        print(p.get('nome_medicamento'))
+        medicamento_usado = medicamento.objects.get(nome_medicamento=p.get('nome_medicamento'))
+        fornecedor_usado = fornecedor.objects.get(nome_fornecedor=p.get('nome_fornecedor'))
+        novo_lote = lote_medicamento(
+            id_medicamento = medicamento_usado,
+            id_fornecedor =  fornecedor_usado,
+            preco = p.get('preco'),
+            quantidade_de_caixas = p.get('quantidade_de_caixas'),
+            quantidade_por_caixa = p.get('quantidade_por_caixa') + ' ' + p.get('unidade_quantidade_por_caixa'),
+            dosagem = p.get('dosagem') + ' ' + p.get('unidade_dosagem'),
+            data_de_validade = p.get('data_de_validade'),
+            industria_farmaceutica = p.get('industria_farmaceutica'),
+        )
     
+        novo_lote.save()   
 
 
     return render(request,'pagina_de_entrada_de_estoque.html')
