daabb1ad1c5118ad5db2d7aacc50d48f91147a6b
nickolasbmm <nickolasbmm44@gmail.com>
2020-12-08 17:27:40 -0300

    obrigado por apagar o /static/_js/script.js

--- farmacita/controle_estoque/urls.py
+++ farmacita/controle_estoque/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     #path('cursos',views.show_courses, name = 'show_courses'),
+    path('entrada_estoque',views.entrada_estoque, name = 'entrada_estoque'),
 ]
--- farmacita/controle_estoque/views.py
+++ farmacita/controle_estoque/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def entrada_estoque(request):
+    
+
+
+    return render(request,'pagina_de_entrada_de_estoque.html')
