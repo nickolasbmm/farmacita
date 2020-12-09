18a8bb7636ff3dea88f7d0c0c9d2c78dfb35d260
renatocraf <renato@renato_craf@yahoo.com.br>
2020-12-09 16:06:48 -0300

    falta arrumar o form de edicao

--- farmacita/cadastro_medicamentos/views.py
+++ farmacita/cadastro_medicamentos/views.py
@@ -22,7 +22,7 @@ def cadastro_medicamentos(request):
     return render(request,'medicamento/pagina_cadastro_medicamento.html',{"droga":droga_list})
 
 def editar_medicamento(request):   
-    if request.method == "POST":
+    '''if request.method == "POST":
         p = request.POST
         id_medicamento = request.id_medicamento
         editarmedicamento = medicamento.objects.filter(id_medicamento = id_medicamento)
@@ -30,9 +30,32 @@ def editar_medicamento(request):
             nome_medicamento = p.get("name"),
             classificacao = p.get("classificacao"), 
             principio_ativo =p.get("principio_ativo")
-        )
-    return render(request,'medicamento/editar_medicamento.html')
+        )'''
+  
+    lista = medicamento.objects.all()
+    editar = False
+    
+    busca = request.GET.get('buscamedic')
 
+    if busca:
+        editar = False
+        lista = medicamento.objects.filter(nome_medicamento__icontains = busca)
+
+    delete = request.GET.get('delete')
+    
+    if delete:            
+        teste = medicamento.objects.filter(id_medicamento = delete)
+        teste.update(excluido = True)
+    
+    editando = request.GET.get('edit')
+    if editando:
+        editar = True
+        lista = medicamento.objects.filter(id_medicamento = editando) 
+
+
+    return render(request,'medicamento/editar_medicamento.html',{'lista':lista,'editar':editar})
+
+'''
 def excluir_medicamento(request):   
     if request.method == "POST":
         id_medicamento = request.id_medicamento
@@ -41,6 +64,7 @@ def excluir_medicamento(request):
            excluido = True
         )
     return render(request,'medicamento/pagina_exclusao_medicamento.html')
+'''
 
 def cadastrar_principio_ativo(request):
     if request.method == "POST":
