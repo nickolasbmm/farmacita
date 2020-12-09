241f267a7070052e4f3cde57ee02ea9b0f9b9fb8
nandaasrodrigues <50269159+nandaasrodrigues@users.noreply.github.com>
2020-12-09 02:24:38 -0300

    Update models.py

--- farmacita/financeiro/models.py
+++ farmacita/financeiro/models.py
@@ -3,9 +3,29 @@ from pessoas.models import cliente
 from controle_estoque.models import lote_medicamento
 
 # Create your models here.
-
+'''
 class ordem_de_venda(models.Model):
     id_ordem_de_venda = models.AutoField(primary_key=True)
     id_cliente = models.ForeignKey(cliente,on_delete=models.PROTECT)
     id_lote_medicamento = models.ForeignKey(lote_medicamento,on_delete=models.PROTECT)
     desconto = models.DecimalField(max_digits=40,decimal_places=2)
+'''
+
+class ordem_de_venda(models.Model):
+    id_ordem_de_venda = models.AutoField(primary_key=True)
+    id_cliente = models.ForeignKey(cliente,on_delete=models.PROTECT)
+    id_lote_medicamento = models.ForeignKey(lote_medicamento,on_delete=models.PROTECT)
+    quantidade = models.IntegerField()
+    desconto =  models.BooleanField(default=False)
+    venda =  models.BooleanField(default=False)
+    ativo = models.BooleanField(default=True)
+    data_de_venda = models.DateTimeField(auto_now=False,auto_now_add=False,null=True)
+
+class ordem_de_compra(models.Model):
+    id_ordem_de_compra = models.AutoField(primary_key=True)
+    id_fornecedor = models.ForeignKey(fornecedor,on_delete=models.PROTECT)
+    id_medicamento = models.ForeignKey(medicamento,on_delete=models.PROTECT)
+    preco_lote = models.DecimalField(max_digits=40,decimal_places=2)
+    quantidade_lote = models.IntegerField()
+    data_de_compra = models.DateTimeField(auto_now=True,auto_now_add=True, null=False)
+    
