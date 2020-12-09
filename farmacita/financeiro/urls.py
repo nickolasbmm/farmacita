d88cc6bd6ae1a188d7b71d2e9e74b7a9065a020e
nandaasrodrigues <50269159+nandaasrodrigues@users.noreply.github.com>
2020-12-09 02:38:07 -0300

    Update urls.py

--- farmacita/financeiro/urls.py
+++ farmacita/financeiro/urls.py
@@ -3,14 +3,6 @@ from . import views
 
 urlpatterns = [
     #path('cursos',views.show_courses, name = 'show_courses'),
-    path('authentication',views.authentication, name = 'authentication'),
-]
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    #path('cursos',views.show_courses, name = 'show_courses'),
     path('criar_ordem_de_venda',views.criar_ordem_de_venda, name = 'criar_ordem_de_venda'),
     path('desistir_compra',views.desistir_compra, name = 'desistir_compra'),
     path('vender_medicamento',views.vender_medicamento, name = 'vender_medicamento'),
