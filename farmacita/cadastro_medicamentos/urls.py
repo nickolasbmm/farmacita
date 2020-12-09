b65c0dd665213fd997a7bfa03d0cf3f3e067cb00
nandaasrodrigues <50269159+nandaasrodrigues@users.noreply.github.com>
2020-12-08 23:19:06 -0300

    Update urls.py

--- farmacita/cadastro_medicamentos/urls.py
+++ farmacita/cadastro_medicamentos/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.cadastro_medicamentos, name = 'cadastro_medicamentos')
+    path('cadastro_medicamentos',views.cadastro_medicamentos, name = 'cadastro_medicamentos'),
+    path('editar_medicamento',views.editar_medicamento, name = 'editar_medicamento'),
+    path('excluir_medicamento',views.excluir_medicamento, name = 'excluir_medicamento')
 ]
