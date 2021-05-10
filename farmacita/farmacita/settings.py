c6d137668fb38282a30120aa0738d2b623db5779
nickolasbmm <nickolasbmm44@gmail.com>
2021-05-10 18:16:31 -0300

    datatable lote

--- farmacita/farmacita/settings.py
+++ farmacita/farmacita/settings.py
@@ -112,13 +112,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
 
@@ -131,3 +131,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+DATE_FORMAT = 'd/m/Y'
+
+DATE_INPUT_FORMATS = (
+    '%d/%m/%Y',
+)
