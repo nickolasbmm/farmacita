6e090237ae22a4b944c085dc80b12733af23c14f
Samarasrs <smr.samara.srs@gmail.com>
2021-05-21 16:46:12 -0300

    Importando csv MEDICAMENTO

--- farmacita/cadastro_medicamentos/admin.py
+++ farmacita/cadastro_medicamentos/admin.py
@@ -1,9 +1,41 @@
+import json
 from django.contrib import admin
-from .models import medicamento, principio_ativo, principio_ativo2, rel_medicamento_principio_ativo2
+from .models import medicamento, principio_ativo2, rel_medicamento_principio_ativo2
+from import_export import fields, resources
+from import_export.admin import ImportExportModelAdmin
+from import_export.widgets import JSONWidget, ManyToManyWidget
 
 # Register your models here.
 
-admin.site.register(medicamento)
-admin.site.register(principio_ativo)
-admin.site.register(principio_ativo2)
+
+class MedicamentoResource(resources.ModelResource):
+    principio_ativo = fields.Field(attribute ='principio_ativo',
+                                    widget=ManyToManyWidget(principio_ativo2, separator = ';', field = 'nome_principio_ativo2'))
+
+
+    class Meta:
+        model = medicamento
+        import_id_fields = ('nome_medicamento',)
+        fields = ['nome_medicamento',  'classificacao','principio_ativo']
+
+
+class MedicamentoDataAdmin(ImportExportModelAdmin):
+    resource_class = MedicamentoResource
+
+admin.site.register(medicamento, MedicamentoDataAdmin)
+
+class PrincipioAtivoResource(resources.ModelResource):
+    
+    class Meta:
+        model = principio_ativo2
+        import_id_fields = ('nome_principio_ativo2',)
+        exclude = ['id_principio_ativo2']
+        
+
+class PrincipioAtivoDataAdmin(ImportExportModelAdmin):
+    resource_class = PrincipioAtivoResource
+
+admin.site.register(principio_ativo2, PrincipioAtivoDataAdmin)
+
+
 admin.site.register(rel_medicamento_principio_ativo2)
