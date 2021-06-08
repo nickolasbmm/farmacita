ef8d9d3e1149c9d1d50c9ccfc6d52d75c1734936
nandaasrodrigues <50269159+nandaasrodrigues@users.noreply.github.com>
2021-06-08 17:02:43 -0300

    Update urls.py

--- farmacita/pessoas/urls.py
+++ farmacita/pessoas/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('editar_usuario',views.editar_usuario, name = 'editar_usuario'),
     path('editar_fornecedor',views.editar_fornecedor, name = 'editar_fornecedor'),
     # path('compras_cliente',views.compras_cliente, name = 'compras_cliente'),
-    path('gerar_relatorio2',views.gerar_relatorio2, name = 'gerar_relatorio2'),
-    path('deslogar',views.deslogar,name='deslogar')
+    path('deslogar',views.deslogar,name='deslogar'),
+    path('gerar_relatorio_pessoas',views.gerar_relatorio_pessoas, name = 'gerar_relatorio_pessoas')
 ]
