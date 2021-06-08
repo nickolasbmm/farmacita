64777e968bbd676a3cc489daf212d3adf39c3cca
nandaasrodrigues <50269159+nandaasrodrigues@users.noreply.github.com>
2021-06-08 18:20:07 -0300

    Update views.py

--- farmacita/financeiro/views.py
+++ farmacita/financeiro/views.py
@@ -440,14 +440,16 @@ def gerar_relatorio_vendas(request):
     values('id_lote_medicamento__id_medicamento__nome_medicamento', \
     quant_ant = Sum('quantidade')).order_by('id_lote_medicamento__id_medicamento__nome_medicamento')
 
+    
     vendas = ordem_de_venda.objects.filter(ativo=True,venda=True, data_de_venda__range = [data_ini, data_fin]). \
     select_related('id_lote_medicamento', 'id_cliente').select_related('id_medicamento').\
     values('id_lote_medicamento__id_medicamento__nome_medicamento', \
     idadeS = Sum((ExtractYear(datetime.now()) - ExtractYear('id_cliente__data_nascimento'))*F('quantidade'), output_Field = FloatField())). \
     annotate(quant = Sum('quantidade', output_Field = FloatField()), \
     valor = Sum('valor_total_venda', output_Field = FloatField()), 
-    descS = Sum(F('percentual_desconto')*F('quantidade'), output_Field = FloatField()),
+    aux = ExpressionWrapper(F('percentual_desconto')*F('quantidade'), output_field=FloatField()), \
     diaS = Sum(ExtractDay('data_de_venda')*F('quantidade'), output_Field = FloatField())). \
+    annotate(descS = Sum('aux')). \ 
     annotate( avg = ExpressionWrapper( F('valor')/F('quant'), output_field=FloatField())). \
     annotate( desc = ExpressionWrapper( F('descS')/F('quant'), output_field=FloatField())). \
     annotate( dia = ExpressionWrapper( F('diaS')/F('quant'), output_field=FloatField())). \
